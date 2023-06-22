0871afd4634fa8d94bfa27ee9e2f5b6d4f8aaf6f
lontten <lontten@163.com>
2023-06-22 10:31:33 +0800

    注释

--- core/components/LnForm/lnFormType.ts
+++ core/components/LnForm/lnFormType.ts
@@ -6,10 +6,10 @@ export interface LnFormPropsType {
     rule?: any[],
     min?: number,
     max?: number,
-    uploadType?: 'oss' | 'cos' | 'http',
-    useJson?: boolean,
-    fileTypeList?: string[],
-    fileSizeMax?:number,
+    uploadType?: 'oss' | 'cos' | 'http',  //文件上传的方式
+    useJson?: boolean, //数组结构的数据是否使用json格式
+    fileTypeList?: string[], //可上传的文件类型列表
+    fileSizeMax?:number, //可上传的最大文件格式大小
 
 
     addressMapType?:AddressMapType //地图类型

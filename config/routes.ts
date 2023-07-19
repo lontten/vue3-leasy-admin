eb2636be167dfa290b118a3eeec58e4d2d07dd7c
lontten <lontten@163.com>
2023-07-19 10:38:11 +0800

    删除多余路由

--- config/routes.ts
+++ config/routes.ts
@@ -1,10 +1,6 @@
 import Home from "@/pages_demo/home/Home.vue";
 import Login from "@/pages_demo/login/Login.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
-import TestPage from "@/pages_test/proIcon/TestPage.vue";
-import TestNavTree from "@/pages_test/navTree/TestNavTree.vue";
-import TestProForm from "@/pages_test/proForm/TestProForm.vue";
-import TestProTables from "@/pages_test/testTables/TestProTables.vue";
 import TestTxMap from "@/pages_test/testTxMap/TestTxMap.vue";
 import p404 from "@/pages_demo/sys/p404.vue";
 import p500 from "@/pages_demo/sys/p500.vue";
@@ -24,10 +20,6 @@ export const routes = [
     },
     {path: '/login', component: Login},
     {path: '/hello', component: HelloWorld},
-    {path: '/testProIcon', component: TestPage},
-    {path: '/testNavTree', component: TestNavTree},
-    {path: '/testProForm', component: TestProForm},
-    {path: '/testProTables', component: TestProTables},
     {path: '/404', component: p404},
     {path: '/403', component: p403},
     {path: '/500', component: p500},

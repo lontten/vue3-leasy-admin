81fcf71d43fe48506b96f42d12502639efbad101
lontten <lontten@163.com>
2023-07-20 17:54:55 +0800

    feat: demo_page 数量概览

--- config/routes.ts
+++ config/routes.ts
@@ -1,13 +1,13 @@
 import Home from "@/pages_demo/home/Home.vue";
 import Login from "@/pages_demo/login/Login.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
-import TestTxMap from "@/pages_test/testTxMap/TestTxMap.vue";
 import p404 from "@/pages_demo/sys/p404.vue";
 import p500 from "@/pages_demo/sys/p500.vue";
 import p403 from "@/pages_demo/sys/p403.vue";
 import pSuccess from "@/pages_demo/sys/pSuccess.vue";
 import pError from "@/pages_demo/sys/pError.vue";
 import testForm from "@/pages_test/testForm.vue";
+import overview from "@/pages_demo/overview/overview.vue";
 
 
 export const routes = [
@@ -15,7 +15,7 @@ export const routes = [
     {
         path: '/home', component: Home,
         children: [
-            {path: 'overview', component: TestTxMap},
+            {path: 'overview', component: overview},
             {path: 'abc', component: testForm},
 
         ]
